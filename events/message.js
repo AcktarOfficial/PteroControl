85bd5aa7f7549edf58cd9e1bd299e18346cfcc70
CodeCannibals <itsmeacktaryt@gmail.com>
2020-10-02 11:49:54 +0400

    Create message.js

---
+++ events/message.js
@@ -0,0 +1,14 @@
+module.exports = (client, message) => {
+const config = require("./config.json");
+
+  if (message.author.bot) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
+  
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  const cmd = client.commands.get(command);
+
+  if (!cmd) return;
+  cmd.run(client, message, args);
+  
+};
