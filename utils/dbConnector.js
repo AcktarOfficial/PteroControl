822bae4b3110b1780f6ab42c257272053ce63c34
CodeCannibals <itsmeacktaryt@gmail.com>
2020-10-02 11:47:21 +0400

    Create dbConnector.js

---
+++ utils/dbConnector.js
@@ -0,0 +1,13 @@
+const { createPool } = require('mysql');
+const config = require("./config.json");
+
+const connection = createPool({
+	connectionLimit: 20,
+	host: config.mysqlHOST,
+	port: '3306',
+	user: config.mysqlUSER,
+	password: config.mysqlPASS,
+	database: config.mysqlDB,
+});
+
+module.exports = connection;
