d6c690f56975693d346fe5b884e574a825e740c9
CodeCannibals <itsmeacktaryt@gmail.com>
2020-10-02 11:48:41 +0400

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,10 @@
+module.exports = (client) => {
+  client.user.setActivity("https://acktaris.fun ¦ afk my website kThksBai");
+  console.log("=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log("Name: PteroControl");
+  console.log("Version: 1.0.0");
+  console.log("Loaded: true");
+  console.log("Developer: AcktarOfficial#6930");
+  console.log("Website: acktarofficial.com");
+  console.log("=+=+=+=+=+=+=+=+=+=+=+=");
+} 
